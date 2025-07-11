0c23cab7d05af73e823d865f1ca981543074210d
Twin <tjssg678@gmail.com>
2025-07-11 17:53:49 -0400

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export let cart = [{
+
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+[{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 },{
@@ -6,6 +8,9 @@ export let cart = [{
   quantity: 1
 }];
 
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId){
     let matchingItem;
@@ -24,6 +29,7 @@ export function addToCart(productId){
         quatity: 1
       });
     }
+    saveToStorage();
 
 }
 
@@ -38,5 +44,7 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 
 }
