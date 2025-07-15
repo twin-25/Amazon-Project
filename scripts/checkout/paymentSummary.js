6de04fce0014ccb973ab9a6ef8353ef3259a100e
Twin <tjssg678@gmail.com>
2025-07-15 10:56:39 -0400

    make items quantity  dynamic in payment summary

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -7,11 +7,14 @@ import {formatCurrency} from '../utils/money.js';
 export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
+  let cartQuantity = 0;
+  
 
 
   cart.forEach((cartItem) =>{
     const product = getProduct(cartItem.productId);
     productPriceCents += product.priceCents * cartItem.quantity;
+    cartQuantity += cartItem.quantity;
 
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
     shippingPriceCents += deliveryOption.priceCents;
@@ -28,7 +31,7 @@ export function renderPaymentSummary() {
           </div>
 
           <div class="payment-summary-row">
-            <div>Items ():</div>
+            <div>Items (${cartQuantity}):</div>
             <div class="payment-summary-money">
             $${formatCurrency(productPriceCents)}
             </div>
