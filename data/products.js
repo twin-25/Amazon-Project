d4d69d04715ea30690dca3ebab81de42932d7261
Twin <tjssg678@gmail.com>
2025-08-04 15:22:41 -0400

    use inheritance and polymorphism to include size chart for clothing products

--- data/products.js
+++ data/products.js
@@ -38,8 +38,32 @@
     return `$${formatCurrency(this.priceCents)}`
     
   }
+
+  extraInfoHTML() {
+    return ``;
+  }
  }
- 
+
+ class Clothing extends Product {
+  sizeChartLink;
+
+  constructor (productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target = "_blank" >Size Chart</a>
+    
+    `
+  }
+
+ }
+
+
  
  export const products = [
   {
@@ -701,7 +725,9 @@
     ]
   }
 ].map((productDetails) => {
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
-console.log(products);
